a4ff82ecd0159e3ce820412c127876a2fa093ac1
Jan Jörg <jan.joerg.jj@gmail.com>
2025-03-23 17:31:25 +0100

    umsatz bugfixes und kleine Ergänzungen

--- frontend/src/routes/revenue/+page.server.ts
+++ frontend/src/routes/revenue/+page.server.ts
@@ -12,7 +12,7 @@ export const load: PageServerLoad = async () => {
 
     const totalDrinkAmount: { total_earnings: number }[] =
       await prismaClient.$queryRaw(
-        Prisma.sql`SELECT SUM(do.amount * d.price) AS total_earnings FROM DrinkOrder do JOIN Drink d ON mo.menuId = m.id`
+        Prisma.sql`SELECT SUM(do.amount * d.price) AS total_earnings FROM DrinkOrder do JOIN Drink d ON do.drinkId = d.id`
       );
 
     const totalAmount =
@@ -20,11 +20,25 @@ export const load: PageServerLoad = async () => {
 
     const ordersAmount = await prismaClient.order.count();
 
+    const drinkOrdersAmount = await prismaClient.drinkOrder.aggregate({
+      _sum: {
+        amount: true,
+      },
+    });
+
+    const menuOrdersAmount = await prismaClient.menuOrder.aggregate({
+      _sum: {
+        amount: true,
+      },
+    });
+
     return {
-      totalMenuAmount,
-      totalDrinkAmount,
+      totalMenuAmount: totalMenuAmount[0].total_earnings,
+      totalDrinkAmount: totalDrinkAmount[0].total_earnings,
       totalAmount,
       ordersAmount,
+      drinkOrdersAmount: drinkOrdersAmount._sum.amount,
+      menuOrdersAmount: menuOrdersAmount._sum.amount,
     };
   } catch (err) {
     console.error("Error fetching total amounts:", err);
--- frontend/src/routes/revenue/+page.svelte
+++ frontend/src/routes/revenue/+page.svelte
@@ -6,22 +6,28 @@
 
 <div class="m-2"></div>
 <div class="grid grid-cols-2 max-w-md">
-  <p>Anzahl Bestellungen:</p>
-  <p class="text-end">{data.ordersAmount}</p>
+  <p>Anzahl Bestellte Getränke:</p>
+  <p class="text-end">{data.drinkOrdersAmount}</p>
   <p>Getränke:</p>
   <p class="text-end">
     {Intl.NumberFormat("de-CH", {
       style: "currency",
       currency: "CHF",
-    }).format(data.totalDrinkAmount[0].total_earnings)}
+    }).format(data.totalDrinkAmount)}
   </p>
+
+  <p>Anzahl Bestellte Menus:</p>
+  <p class="text-end">{data.menuOrdersAmount}</p>
   <p>Menus:</p>
   <p class="text-end">
     {Intl.NumberFormat("de-CH", {
       style: "currency",
       currency: "CHF",
-    }).format(data.totalMenuAmount[0].total_earnings)}
+    }).format(data.totalMenuAmount)}
   </p>
+
+  <p>Total Bestellungen:</p>
+  <p class="text-end">{data.ordersAmount}</p>
 
   <p class="text-lg font-bold">Total:</p>
   <p class="text-lg font-bold text-end">
